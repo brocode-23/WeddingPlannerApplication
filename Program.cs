f7f35515fa724e9a1fd44fc7e4c97ffe1497580f
brocode-23 <kavindumihirangabandara@gmail.com>
2025-04-13 22:10:15 +0530

    add comment

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production dfdfdfdf scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
