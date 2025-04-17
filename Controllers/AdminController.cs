6d7753d9666f16675a40ee9ccdbfcabe37a63e39
brocode-23 <kavindumihirangabandara@gmail.com>
2025-04-17 21:29:10 +0530

    Edited userinterafaces

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeddingPlannerApplication.Data;
 
 namespace WeddingPlannerApplication.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
+        public async Task<IActionResult> Index()
+        {
+            var bookings = await _context.Bookings
+                .Include(b => b.Vendor)
+                .Include(b => b.Payments)
+                .Include(b => b.Reviews)
+                .Where(b => !b.IsDeleted)
+                .ToListAsync();
+
+            ViewBag.TotalBookings = bookings.Count;
+            ViewBag.TotalRevenue = bookings.Sum(b => b.TotalAmount);
+            ViewBag.PendingPayments = bookings.Count(b => b.PaymentStatus == "Pending");
+            ViewBag.CancelledBookings = bookings.Count(b => b.Status == "Cancelled");
+
+            return View(bookings);
+        }
+
     }
 }
 
--- Controllers/PlannerUserController.cs
+++ Controllers/PlannerUserController.cs
@@ -1,4 +1,4 @@
-﻿using DreamDayWeddingPlanner.Models;
+﻿
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
